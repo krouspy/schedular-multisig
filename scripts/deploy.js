e9f7f6d7f0b1dd614210d97bb6dec778bf416f40
krouspy <knjlau1@gmail.com>
2021-08-05 14:01:11 +0200

    upgrade deploy scripts
    add proxy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,6 +5,7 @@ async function main() {
 
   const StorageV1 = await hre.ethers.getContractFactory('StorageV1');
   const StorageV2 = await hre.ethers.getContractFactory('StorageV2');
+  const Proxy = await hre.ethers.getContractFactory('UpgradeabilityProxy');
 
   const storageV1 = await StorageV1.deploy();
   const storageV2 = await StorageV2.deploy();
@@ -12,8 +13,12 @@ async function main() {
   await storageV1.deployed();
   await storageV2.deployed();
 
+  const proxy = await Proxy.deploy(storageV1.address);
+  await proxy.deployed();
+
   console.log('StorageV1 deployed to:', storageV1.address);
   console.log('StorageV2 deployed to:', storageV2.address);
+  console.log('Proxy deployed to:', proxy.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
